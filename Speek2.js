347ea198131ec4845c7593bd4f884f80993a47b7
envym <38915828+envym@users.noreply.github.com>
2018-06-14 14:37:14 +0300

    Add files via upload

--- Speek2.js
+++ Speek2.js
@@ -13,7 +13,7 @@
     var result = event.results[event.resultIndex];
     if (result.isFinal) {
      // document.write('Вы сказали: ' + result[0].transcript);
-     el.innerText = result[0].transcript;
+      document.getElementById("my").innerHTML = result[0].transcript ;
     } else {
       console.log('Промежуточный результат: ', result[0].transcript);
     }
@@ -28,8 +28,7 @@
   var utterance = new SpeechSynthesisUtterance('How about we say this now? This is quite a long sentence to say.');
 
   function talk () {
-    synth.speak (utterance);
-    document.write(result[0].transcript);
+    //synth.speak (utterance);
   }
 
   function stop () {
