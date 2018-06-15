a4a8949e2d80244596d393e1da25590d9cc12276
envym <38915828+envym@users.noreply.github.com>
2018-06-15 08:46:18 +0300

    Add files via upload

--- Speek2.js
+++ Speek2.js
@@ -25,14 +25,14 @@
     recognizer.start();
   }
 
-  var synth = window.speechSynthesis;
+  /*var synth = window.speechSynthesis;
   var utterance = new SpeechSynthesisUtterance('How about we say this now? This is quite a long sentence to say.');
 
-/*  function talk () {
+  function talk () {
     //synth.speak (utterance);
-  }*/
+  }
 
   function stop () {
     synth.pause();
-  }
+  }*/
 
