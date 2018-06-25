ba73ed2044b3258410a04ab8ff55d17f00f60424
envym <38915828+envym@users.noreply.github.com>
2018-06-26 00:11:38 +0300

    Add files via upload

--- Speek2.js
+++ Speek2.js
@@ -13,7 +13,8 @@
     var result = event.results[event.resultIndex];
    document.getElementById("my").innerHTML = result[0].transcript;
     if (result.isFinal) {
-      document.getElementById('my').value += "\r\n";
+      //document.getElementById('my').value += "\r\n";
+      
     } else {
       console.log('Промежуточный результат: ', result[0].transcript);
     }
