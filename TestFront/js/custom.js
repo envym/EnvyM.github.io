42a30ec2045493997e66427d1a265923fe323692
envym <38915828+envym@users.noreply.github.com>
2022-01-23 06:52:31 +0200

    Add files via upload

--- TestFront/js/custom.js
+++ TestFront/js/custom.js
@@ -1,3 +1,6 @@
+
+let mobileLenghPX = 620;
+
 function changeErroDisplay(){
 
 
@@ -219,7 +222,6 @@ function changeErroDisplay(){
 		document.getElementById("country_open_icon").style.display = 'none';
 		document.getElementById("country_close_icon").style.display = 'flex';
 		document.getElementById('Country').removeAttribute("onclick");
-		document.getElementById('trigger_form_field_Country').style.overflow = 'visible';
 
 	}
 
@@ -237,6 +239,17 @@ function changeErroDisplay(){
 
 	function countryCreate(){
 		let contry_name = ['Afghanistan', 	'Albania', 	'Algeria', 	'American Samoa', 	'Andorra', 	'Angola', 	'Anguilla', 	'Antigua and Barbuda', 	'Armenia', 	'Argentina', 	'Australia', 	'Austria', 	'Azerbaijan', 	'Bahamas', 	'Bahrain', 	'Bangladesh', 	'Barbados', 	'Belarus', 	'Belgium', 	'Belize', 	'Benin', 	'Bermuda', 	'Bolivia', 	'Bosnia and Herzegovina', 	'Botswana', 	'Brazil', 	'British Virgin Islands', 	'Brunei Darusalaam', 	'Bulgaria', 	'Burkina Faso', 	'Burundi', 	'Byelorussian', 	'Cambodia', 	'Cameroon', 	'Canada', 	'Cape Verde', 	'Cayman Islands', 	'Central African Republic', 	'Chad', 	'Chile', 	'China', 	'Christmas Island', 	'Cocos Islands', 	'Colombia', 	'Commonwealth of the Northern Mariana Islands', 	'Comoros and Mayotte Island', 	'Congo', 	'Cook Islands', 	'Costa Rica', 	'Croatia', 	'Cuba', 	'Cyprus', 	'Czech Republic', 	'Denmark', 	'Diego Garcia', 	'Djibouti', 	'Dominica', 	'Dominican Republic', 	'East Timor', 	'Ecuador', 	'Egypt', 	'El Salvador', 	'Equatorial Guinea', 	'Estonia', 	'Ethiopia', 	'Faeroe Islands', 	'Falkland Islands', 	'Fiji', 	'Finland', 	'France', 	'French Antilles', 	'French Guiana', 	'French Polynesia', 	'Gabon', 	'Gambia', 	'Georgia', 	'Germany', 	'Ghana', 	'Gibraltar', 	'Greece', 	'Greenland', 	'Grenada', 	'Guam', 	'Guatemala', 	'Guinea', 	'Guinea-Bissau', 	'Guyana', 	'Haiti', 	'Honduras', 	'Hong Kong', 	'Hungary', 	'Iceland', 	'India', 	'Indonesia', 	'Iran', 	'Iraq', 	'Irish Republic', 	'Israel', 	'Italy', 	'Ivory Coast', 	'Jamaica', 	'Japan', 	'Jordan', 	'Kazakhstan', 	'Kenya', 	'Kiribati Republic', 	'Kirg(h)izia', 	'Kuwait', 	'Laos', 	'Latvia', 	'Lebanon', 	'Lesotho', 	'Liberia', 	'Libya', 	'Liechtenstein', 	'Lithuania', 	'Luxembourg', 	'Macao', 	'Macedonia', 	'Madagascar', 	'Malawi', 	'Malaysia', 	'Maldives', 	'Mali', 	'Malta', 	'Marshall Islands', 	'Martinique', 	'Mauritania', 	'Mauritius', 	'Mexico', 	'Micronesia', 	'Monaco', 	'Mongolia', 	'Montserrat', 	'Morocco', 	'Mozambique', 	'Myanmar', 	'Namibia', 	'Nauru', 	'Nepal', 	'Netherlands', 	'Netherlands Antilles', 	'New Caledonia', 	'New Zealand', 	'Nicaragua', 	'Niger', 	'Nigeria', 	'Niue', 	'Norfolk Island', 	'North Korea', 	'North Yemen', 	'Northern Mariana Islands', 	'Norway', 	'Oman', 	'Pakistan', 	'Panama', 	'Papua New Guinea', 	'Paraguay', 	'Peru', 	'Philippines', 	'Poland', 	'Portugal', 	'Puerto Rico', 	'Qatar', 	'Republic of San Marino', 	'Reunion', 	'Romania', 	'Russia', 	'Rwandese Republic', 	'Saint Helena and Ascension Island', 	'Saint Pierre et Miquelon', 	'San Marino', 	'Sao Tome e Principe', 	'Saudi Arabia', 	'Senegal', 	'Seychelles', 	'Sierra Leone', 	'Singapore', 	'Slovakia', 	'Slovenia', 	'Solomon Islands', 	'Somalia', 	'South Africa', 	'South Korea', 	'South Yemen', 	'Spain', 	'Sri Lanka', 	'St.Kitts and Nevis', 	'St.Lucia', 	'St.Vincent and the Grenadines', 	'Sudan', 	'Suriname', 	'Svalbard and Jan Mayen Islands', 	'Swaziland', 	'Sweden', 	'Switzerland', 	'Syria', 	'Ta(d)jikistan', 	'Taiwan', 	'Tanzania', 	'Thailand', 	'Togolese Republic', 	'Tokelau', 	'Tonga', 	'Trinidad and Tobago', 	'Tunisia', 	'Turkey', 	'Turkmenistan', 	'Turks & Caicos Islands', 	'Tuvalu', 	'US Virgin Islands', 	'Uganda', 	'Ukraine', 	'United Arab Emirates', 	'United Kingdom', 	'Uruguay', 	'USA', 	'Uzbekistan', 	'Vanuatu', 	'Vatican City', 	'Venezuela', 	'Vietnam', 	'Wallis and Futuna Islands', 	'Western Sahara', 	'Western Samoa', 	'Yugoslavia', 	'Zaire', 	'Zambia', 	'Zimbabwe'];
+		let windWidth = window.innerWidth;
+		alert( windWidth);
+
+		if (windWidth < mobileLenghPX){
+			document.getElementById("Country_sel").size = 0;
+
+		}
+		else{
+			null;
+		}
+
 
 		for(i = contry_name.length - 1; i >= 0; i = i-1){
 			let liFirst = document.createElement('option');
@@ -254,10 +267,16 @@ function changeErroDisplay(){
 		
 
 		document.getElementById("Country").value = document.getElementById("Country_sel").value;
-
-		document.getElementById("Country_sel").style.display = 'none';
-		document.getElementById("country_open_icon").style.display = 'flex';
-		document.getElementById("country_close_icon").style.display = 'none';
+		let windWidth = window.innerWidth;
+		if(windWidth > mobileLenghPX){
+			document.getElementById("Country_sel").style.display = 'none';
+			document.getElementById("country_open_icon").style.display = 'flex';
+			document.getElementById("country_close_icon").style.display = 'none';
+		}
+		else{
+			null;
+		}
+		
 
 		let contry_phone = ['+93', 	'+355', '+21', 	'+684', 	'+376', '+244', 	'+1-264', 	'+1-268', 	'+374', 	'+54', 	'+61', 	'+43', 	'+994', 	'+1-242', 	'+973', 	'+880', 	'+1-246', 	'+375', 	'+32', 	'+501', 	'+229', 	'+1-441', 	'+591', 	'+387', 	'+267', 	'+55', 	'+1-284', 	'+673', 	'+359', 	'+226', 	'+257', 	'+7', 	'+855', 	'+237', 	'+1', 	'+238', 	'+1-345', 	'+236', 	'+235', 	'+56', 	'+86', 	'+672', 	'+672', 	'+57', 	'+1-670', 	'+269', 	'+242', 	'+682', 	'+506', 	'+385', 	'+53', 	'+357', 	'+420', 	'+45', 	'+246', 	'+253', 	'+1-767', 	'+1-809', 	'+62', 	'+593', 	'+20', 	'+503', 	'+240', 	'+372', 	'+251', 	'+298', 	'+500', 	'+679', 	'+358', 	'+33', 	'+590', 	'+594', 	'+689', 	'+241', 	'+220', 	'+995', 	'+49', 	'+233', 	'+350', 	'+30', 	'+299', 	'+1-473', 	'+1-671 ', 	'+502', 	'+224', 	'+245', 	'+592', 	'+509', 	'+504', 	'+852', 	'+36', 	'+354', 	'+91', 	'+62', 	'+98', 	'+964', 	'+353', 	'+972', 	'+39', 	'+225', 	'+1-876', 	'+81', 	'+962', 	'+7', 	'+254', 	'+686', 	'+996', 	'+965', 	'+856', 	'+371', 	'+961', 	'+266', 	'+231', 	'+21', 	'+41', 	'+370', 	'+352', 	'+853', 	'+389', 	'+261', 	'+265', 	'+60', 	'+960', 	'+223', 	'+356', 	'+692', 	'+596', 	'+222', 	'+230', 	'+1-706', 	'+691', 	'+377', 	'+976', 	'+1-664', 	'+212', 	'+258', 	'+95', 	'+264', 	'+674', 	'+977', 	'+31', 	'+599', 	'+687', 	'+64', 	'+505', 	'+227', 	'+234', 	'+683', 	'+672', 	'+850', 	'+967', 	'+670', 	'+47', 	'+968', 	'+92', 	'+507', 	'+675', 	'+595', 	'+51', 	'+63', 	'+48', 	'+351', 	'+1-787', 	'+974', 	'+378', 	'+262', 	'+40', 	'+7', 	'+250', 	'+247', 	'+508', 	'+39', 	'+239', 	'+966', 	'+221', 	'+248', 	'+232', 	'+65', 	'+421', 	'+386', 	'+677', 	'+252', 	'+27', 	'+82', 	'+969', 	'+34', 	'+94', 	'+1-869', 	'+1-758', 	'+1-784', 	'+249', 	'+597', 	'+47', 	'+268', 	'+46', 	'+41', 	'+963', 	'+992', 	'+886', 	'+255', 	'+66', 	'+228', 	'+690', 	'+676', 	'+1-868', 	'+21', 	'+90', 	'+993', 	'+1-649', 	'+688', 	'+1-340', 	'+256', 	'+380', 	'+971', 	'+44', 	'+598', 	'+1', 	'+998', 	'+678', 	'+39', 	'+58', 	'+84', 	'+681', 	'+21', 	'+685', 	'+381', 	'+243', 	'+260', 	'+263'];
 		let contry_name = ['Afghanistan', 	'Albania', 	'Algeria', 	'American Samoa', 	'Andorra', 	'Angola', 	'Anguilla', 	'Antigua and Barbuda', 	'Armenia', 	'Argentina', 	'Australia', 	'Austria', 	'Azerbaijan', 	'Bahamas', 	'Bahrain', 	'Bangladesh', 	'Barbados', 	'Belarus', 	'Belgium', 	'Belize', 	'Benin', 	'Bermuda', 	'Bolivia', 	'Bosnia and Herzegovina', 	'Botswana', 	'Brazil', 	'British Virgin Islands', 	'Brunei Darusalaam', 	'Bulgaria', 	'Burkina Faso', 	'Burundi', 	'Byelorussian', 	'Cambodia', 	'Cameroon', 	'Canada', 	'Cape Verde', 	'Cayman Islands', 	'Central African Republic', 	'Chad', 	'Chile', 	'China', 	'Christmas Island', 	'Cocos Islands', 	'Colombia', 	'Commonwealth of the Northern Mariana Islands', 	'Comoros and Mayotte Island', 	'Congo', 	'Cook Islands', 	'Costa Rica', 	'Croatia', 	'Cuba', 	'Cyprus', 	'Czech Republic', 	'Denmark', 	'Diego Garcia', 	'Djibouti', 	'Dominica', 	'Dominican Republic', 	'East Timor', 	'Ecuador', 	'Egypt', 	'El Salvador', 	'Equatorial Guinea', 	'Estonia', 	'Ethiopia', 	'Faeroe Islands', 	'Falkland Islands', 	'Fiji', 	'Finland', 	'France', 	'French Antilles', 	'French Guiana', 	'French Polynesia', 	'Gabon', 	'Gambia', 	'Georgia', 	'Germany', 	'Ghana', 	'Gibraltar', 	'Greece', 	'Greenland', 	'Grenada', 	'Guam', 	'Guatemala', 	'Guinea', 	'Guinea-Bissau', 	'Guyana', 	'Haiti', 	'Honduras', 	'Hong Kong', 	'Hungary', 	'Iceland', 	'India', 	'Indonesia', 	'Iran', 	'Iraq', 	'Irish Republic', 	'Israel', 	'Italy', 	'Ivory Coast', 	'Jamaica', 	'Japan', 	'Jordan', 	'Kazakhstan', 	'Kenya', 	'Kiribati Republic', 	'Kirg(h)izia', 	'Kuwait', 	'Laos', 	'Latvia', 	'Lebanon', 	'Lesotho', 	'Liberia', 	'Libya', 	'Liechtenstein', 	'Lithuania', 	'Luxembourg', 	'Macao', 	'Macedonia', 	'Madagascar', 	'Malawi', 	'Malaysia', 	'Maldives', 	'Mali', 	'Malta', 	'Marshall Islands', 	'Martinique', 	'Mauritania', 	'Mauritius', 	'Mexico', 	'Micronesia', 	'Monaco', 	'Mongolia', 	'Montserrat', 	'Morocco', 	'Mozambique', 	'Myanmar', 	'Namibia', 	'Nauru', 	'Nepal', 	'Netherlands', 	'Netherlands Antilles', 	'New Caledonia', 	'New Zealand', 	'Nicaragua', 	'Niger', 	'Nigeria', 	'Niue', 	'Norfolk Island', 	'North Korea', 	'North Yemen', 	'Northern Mariana Islands', 	'Norway', 	'Oman', 	'Pakistan', 	'Panama', 	'Papua New Guinea', 	'Paraguay', 	'Peru', 	'Philippines', 	'Poland', 	'Portugal', 	'Puerto Rico', 	'Qatar', 	'Republic of San Marino', 	'Reunion', 	'Romania', 	'Russia', 	'Rwandese Republic', 	'Saint Helena and Ascension Island', 	'Saint Pierre et Miquelon', 	'San Marino', 	'Sao Tome e Principe', 	'Saudi Arabia', 	'Senegal', 	'Seychelles', 	'Sierra Leone', 	'Singapore', 	'Slovakia', 	'Slovenia', 	'Solomon Islands', 	'Somalia', 	'South Africa', 	'South Korea', 	'South Yemen', 	'Spain', 	'Sri Lanka', 	'St.Kitts and Nevis', 	'St.Lucia', 	'St.Vincent and the Grenadines', 	'Sudan', 	'Suriname', 	'Svalbard and Jan Mayen Islands', 	'Swaziland', 	'Sweden', 	'Switzerland', 	'Syria', 	'Ta(d)jikistan', 	'Taiwan', 	'Tanzania', 	'Thailand', 	'Togolese Republic', 	'Tokelau', 	'Tonga', 	'Trinidad and Tobago', 	'Tunisia', 	'Turkey', 	'Turkmenistan', 	'Turks & Caicos Islands', 	'Tuvalu', 	'US Virgin Islands', 	'Uganda', 	'Ukraine', 	'United Arab Emirates', 	'United Kingdom', 	'Uruguay', 	'USA', 	'Uzbekistan', 	'Vanuatu', 	'Vatican City', 	'Venezuela', 	'Vietnam', 	'Wallis and Futuna Islands', 	'Western Sahara', 	'Western Samoa', 	'Yugoslavia', 	'Zaire', 	'Zambia', 	'Zimbabwe'];
@@ -383,8 +402,20 @@ function changeErroDisplay(){
 				document.getElementById("placeholder_move_Country").style.color = '#67D9B7';
 			}
 		})
-		$("#Phone").on({
+		$("#Country_sel").on({
 			mouseenter: function() {
+				let windWidth = window.innerWidth;
+				if(windWidth < mobileLenghPX){
+					$("#placeholder_move_Country").animate({ marginTop: "-21px"},{duration:300, queue: false});
+					$("#placeholder_move_Country").animate({ fontSize: "11px"},{duration:300, queue: false});
+					$(".movesellector").animate({ marginTop: "8px"},{duration:300, queue: false});
+					document.getElementById("placeholder_move_Country").style.color = '#67D9B7';
+				}
+				else{null;}
+			}
+		})
+		$("#Phone").on({
+			click: function() {
 				$("#placeholder_move_Phone").animate({ marginTop: "-21px"},{duration:300, queue: false});
 				$("#placeholder_move_Phone").animate({ fontSize: "11px"},{duration:300, queue: false});
 				$(this).animate({ marginTop: "8px"},{duration:300, queue: false});
@@ -436,7 +467,7 @@ function changeErroDisplay(){
 	tl2.from('#form-field_button', 0.5, {y : '110%', delay:0.3})
 	
 
-		
+
 	ScrollTrigger.create({
 
 		animation: tl2,
@@ -448,10 +479,14 @@ function changeErroDisplay(){
 
 
 
-function slowScroll (id) {
-    var offset = 0;
-    $('html, body').animate ({
-      scrollTop: $(id).offset ().top - offset
-    }, 100);
-    return false;
-  }
+	function slowScroll (id) {
+		var offset = 0;
+		$('html, body').animate ({
+			scrollTop: $(id).offset ().top - offset
+		}, 100);
+		return false;
+	}
+
+
+
+
