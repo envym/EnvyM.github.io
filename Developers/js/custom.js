c4f67b78b1cf797846fea5c18325d52acd096a6c
envym <38915828+envym@users.noreply.github.com>
2022-03-19 21:46:21 +0200

    Add files via upload

--- Developers/js/custom.js
+++ Developers/js/custom.js
@@ -377,3 +377,19 @@ function menu_mobile_check(){
 		 document.getElementById("menu_list").style.display = 'flex';
 	}
 }
+
+
+/*______________submit______-*/
+
+
+$(window).on("scroll", function(){
+	$('input[name="scroll"]').val($(window).scrollTop());
+});
+ 
+$(document).ready(function(){
+	var p = window.location.search;
+	p = p.match(new RegExp('scroll=([^&=]+)'));
+	if (p) {
+		window.scrollTo(0, p[1]);
+	}	  
+}); 
